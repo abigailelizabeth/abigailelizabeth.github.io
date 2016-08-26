a9156403031b1bb94328e07da16a5d48b092e9d1
abigailelizabeth <abbyeb22@gmail.com>
2016-08-26 17:38:36 -0500

    did more stuff

---
+++ ui.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    alert("no");
+    
+    function myFunction() {
+     alert("hello");
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+$("#downloadPDF").click(function{
+                        alert("not today, sorry!");
+                        });
+});
