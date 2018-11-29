41bfa6074ada390ba5134a79ef168d74bd1080fc
abigailbarron <abbyeb22@gmail.com>
2018-11-29 13:30:06 -0600

    deploy

---
+++ precache-manifest.648bdadf77f8e7abc4c16ddc5fbc3b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89c2b40338d290e43b06",
+    "url": "/static/css/main.72a6fafb.chunk.css"
+  },
+  {
+    "revision": "89c2b40338d290e43b06",
+    "url": "/static/js/main.89c2b403.chunk.js"
+  },
+  {
+    "revision": "dde825e5d8673d072528",
+    "url": "/static/js/1.dde825e5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65770eaef5def99088657aa224317125",
+    "url": "/static/media/abby.65770eae.jpg"
+  },
+  {
+    "revision": "408659bb9394f2ab48f179db5678ca43",
+    "url": "/index.html"
+  }
+];
