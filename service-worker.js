68e831c9f6ce4d7288a1ebc8454e2804f9afe91b
abigailbarron <abbyeb22@gmail.com>
2018-12-05 13:42:09 -0600

    deploy config

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/abigailelizabeth.github.io/precache-manifest.829f51daadf141cae97f01d6e2467275.js"
+  "/abigailelizabeth.github.io/precache-manifest.b5b79d6f856a6249ada7e4359f41577e.js"
 );
 
 workbox.clientsClaim();
