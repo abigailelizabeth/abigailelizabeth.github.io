ec1294a23407285af7815b7ddb75696edee68689
abigailbarron <abbyeb22@gmail.com>
2018-12-12 13:41:19 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/abigailelizabeth.github.io/precache-manifest.7e3f41ae9a320cb3075ca2bc2aff57fd.js"
+  "/abigailelizabeth.github.io/precache-manifest.4f5448e1d4fc09b99d830bf8c1382455.js"
 );
 
 workbox.clientsClaim();
